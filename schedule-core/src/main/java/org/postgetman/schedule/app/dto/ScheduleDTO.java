b56ea7441a04e20591e86f514226bc8ef77da007
postgetman <postgetman@gmail.com>
2017-10-17 17:00:29 +0300

    create ScheduleDTO

--- schedule-core/src/main/java/org/postgetman/schedule/app/dto/ScheduleDTO.java
+++ schedule-core/src/main/java/org/postgetman/schedule/app/dto/ScheduleDTO.java
@@ -1,4 +1,41 @@
 package org.postgetman.schedule.app.dto;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class ScheduleDTO {
+
+    private LocalDate date;
+
+    private LocalTime timeFrom;
+
+    private LocalTime timeTo;
+
+    public ScheduleDTO(){
+
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTimeFrom() {
+        return timeFrom;
+    }
+
+    public void setTimeFrom(LocalTime timeFrom) {
+        this.timeFrom = timeFrom;
+    }
+
+    public LocalTime getTimeTo() {
+        return timeTo;
+    }
+
+    public void setTimeTo(LocalTime timeTo) {
+        this.timeTo = timeTo;
+    }
 }
