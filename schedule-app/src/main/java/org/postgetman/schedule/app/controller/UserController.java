6b6182a28f6854f689acfb02d90529ef66c3fc8b
postgetman <postgetman@gmail.com>
2017-10-17 11:15:05 +0300

    add SwaggerConfiguration,create UserDTO

--- schedule-app/src/main/java/org/postgetman/schedule/app/controller/UserController.java
+++ schedule-app/src/main/java/org/postgetman/schedule/app/controller/UserController.java
@@ -1,6 +1,7 @@
 package org.postgetman.schedule.app.controller;
 
 import org.postgetman.schedule.app.domain.user.User;
+import org.postgetman.schedule.app.dto.UserDTO;
 import org.postgetman.schedule.app.service.UserService;
 import org.postgetman.schedule.app.service.impl.UserServiceImpl;
 import org.springframework.web.bind.annotation.*;
@@ -8,13 +9,19 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private UserService service = new UserServiceImpl();
 
-    @GetMapping(value = "/users")
+    @GetMapping
     public List<User> findAll(){
         return service.findAll();
+    }
+
+    @PostMapping
+    public void saveUser(@RequestBody UserDTO userDTO){
+
     }
 
 
