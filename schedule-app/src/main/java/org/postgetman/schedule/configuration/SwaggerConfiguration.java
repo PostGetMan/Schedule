5702cefd2e6e0859b59d73338e2f41665943ed1d
postgetman <postgetman@gmail.com>
2017-10-17 11:25:20 +0300

    createBaseController

--- schedule-app/src/main/java/org/postgetman/schedule/configuration/SwaggerConfiguration.java
+++ schedule-app/src/main/java/org/postgetman/schedule/configuration/SwaggerConfiguration.java
@@ -1,6 +1,5 @@
 package org.postgetman.schedule.configuration;
 
-import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -8,6 +7,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.function.Predicate;
 
 @Configuration
 @EnableSwagger2
@@ -23,6 +24,6 @@ public class SwaggerConfiguration {
     }
 
     private Predicate<String> apiPath() {
-        return PathSelectors.regex("/.*");
+        return PathSelectors.regex("/.*")::apply;
     }
 }
